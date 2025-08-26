de93367ad08203c378011e08fc29a41755701614
Rishabh Shrivastava <rishabhshrivastava7565@gmail.com>
2025-08-26 23:17:42 +0530

    26-08-2025

---
+++ Core Java/src/in/co/rays/basic/NameChar.java
@@ -0,0 +1,31 @@
+package in.co.rays.basic;
+
+public class NameChar {
+	public static void main(String[] args) {
+
+		String name = "Rishabh";
+		int count = 0; 
+
+		for (int i = 0; i < name.length(); i++) {
+			if ('R' == name.charAt(i) || ) {
+				count++;
+			}
+
+		System.out.println("R = " + count);
+	
+
+
+		}
+	}
+
+}
+
+
+		
+	
+
+	
+
+
+
+
---
+++ Core Java/src/in/co/rays/basic/ReverseString.java
@@ -0,0 +1,20 @@
+package in.co.rays.basic;
+
+public class ReverseString {
+	
+	public static void main(String[] args) {
+		
+		String str = "Rays";
+		
+for (int i = str.length() -1; i >=0; i--) {
+	
+	System.out.print(str.charAt(i));
+	
+			
+			
+			
+			
+		}
+	}
+
+}

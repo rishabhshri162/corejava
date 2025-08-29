8863ef0bdb6422be93a09d4c50f414e0e24a609a
Rishabh Shrivastava <you@example.com>
2025-08-29 15:23:21 +0530

    29-08-2025

---
+++ Core Java/src/in/co/rays/basic/GetDate.java
@@ -0,0 +1,12 @@
+package in.co.rays.basic;
+
+import java.time.LocalDate;
+
+public class GetDate {
+	
+public static void main(String[] args) {
+	LocalDate today = LocalDate.now();
+	System.out.println("Current Date is :- " + today);
+	
+}
+}

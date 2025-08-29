e54d6cb99d5420690ad4ecf7e45faf1271f54538
Rishabh Shrivastava <you@example.com>
2025-08-29 10:30:42 +0530

    29-08-2025

---
+++ Core Java/src/in/co/rays/basic/ReverseAtSamePlace.java
@@ -0,0 +1,30 @@
+package in.co.rays.basic;
+
+public class ReverseAtSamePlace {
+	
+	
+	   public static void main(String[] args) {
+
+			String str = "Rishabh Shrivastava";
+		String[] a = str.split(" ");
+
+			for (int i = 0; i < a.length; i++) {
+
+				for (int j = a[i].length() - 1; j >= 0; j--) {
+
+					System.out.print(a[i].charAt(j));
+
+				}
+
+				System.out.print(" ");
+
+			}
+
+		}
+
+
+
+	}
+
+
+

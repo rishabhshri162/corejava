7ffcb8a330e5b178760f53564054c568a723edf4
Rishabh Shrivastava <rishabhshrivastava7565@gmail.com>
2025-08-30 13:48:10 +0530

    30-08-2025

---
+++ Core Java/src/in/co/rays/basic/TestMath.java
@@ -0,0 +1,18 @@
+package in.co.rays.basic;
+
+public class TestMath {
+	
+	public static void main(String[] args) {
+		 
+			
+		System.out.println(" Mathematics functions");
+		
+	    System.out.println(" Max 2,5:- " + Math.max(2,5)); 
+	    System.out.println(" Min 2,5:- " + Math.min(2,5)); 
+	    System.out.println(" Absolute 3.7:- " + Math.abs(3.7)); 
+	    System.out.println(" Exp 10:- " + Math.exp(10));
+	    System.out.println(" Random Number:- " + Math.random());
+	    System.out.println(" Square Root:- " + Math.sqrt(4));
+	}
+
+}

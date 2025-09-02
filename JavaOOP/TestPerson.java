35acbb48f9fd38e70394059eb41b45bd80cd6bf9
Rishabh Shrivastava <rishabhshrivastava7565@gmail.com>
2025-09-02 15:17:42 +0530

    Updated code

--- JavaOOP/TestPerson.java
+++ JavaOOP/TestPerson.java
@@ -7,15 +7,15 @@ import java.util.Date;
 public class TestPerson {
 	public static void main(String[] args) throws ParseException {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-		Date dob = sdf.parse("01-06-2002");
+
 
 		Person P = new Person();
 		P.setName("Rishabh");
-		P.setDob(dob);
+		P.setDob(sdf.parse("01-06-2002"));
 		P.setAddress("Indore");
 
 		System.out.println("Name:- " + P.getName());
-		System.out.println("DOB:- " + sdf.format(P.getDob()));
+		System.out.println("DOB:- " + P.getDob());
 		System.out.println("Address:- " + P.getAddress());
 		
 		
@@ -30,32 +30,23 @@ public class TestPerson {
 		System.out.println("--------------------");;
 		
 		SimpleDateFormat sdf1 = new SimpleDateFormat("dd-MM-yyyy");
-		Date dob1 = sdf1.parse("11-05-1990");
+		
 
 		Person R = new Person();
 		R.setName("Ram");
-		R.setDob(dob1);
+		R.setDob(sdf1.parse("11-12-2010"));
 		R.setAddress("Delhi");
 
 		System.out.println("Name:- " + R.getName());
-		System.out.println("DOB:- " + sdf1.format(R.getDob()));
+		System.out.println("DOB:- " + R.getDob());
 		System.out.println("Address:- " + R.getAddress());
 		
-		System.out.println();
-		System.out.println("--------------------");;
 		
-
-		SimpleDateFormat sdf2 = new SimpleDateFormat("dd-MM-yyyy");
-		Date dob2 = sdf2.parse("06-12-2010");
-
-		Person S = new Person();
-		S.setName("Rahul");
-		S.setDob(dob2);
-		S.setAddress("Gurgaon");
-
-		System.out.println("Name:- " + S.getName());
-		System.out.println("DOB:- " + sdf2.format(S.getDob()));
-		System.out.println("Address:- " + S.getAddress());
+		if (R.getAge() >= Person.AVG_AGE) {
+			System.out.println(R.getAge() + "  You are eligible to vote");
+		} else {
+			System.out.println(R.getAge()  + "  You are not eligible to vote");
+		}
 		
 		System.out.println();
 		System.out.println("--------------------");;

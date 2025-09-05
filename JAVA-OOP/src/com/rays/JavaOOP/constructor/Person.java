62935b7cac4d2f8aa00411b2f85e98f90b429f96
Rishabh Shrivastava <rishabhshrivastava7565@gmail.com>
2025-09-05 14:36:59 +0530

    updated code

---
+++ JAVA-OOP/src/com/rays/JavaOOP/constructor/Person.java
@@ -0,0 +1,28 @@
+package com.rays.JavaOOP.constructor;
+
+import java.util.Date;
+
+public class Person {
+	private String name;
+	private String address;
+	private Date dob;
+
+	public Person(String name, String address, Date dob) {
+		this.name = name;
+		this.address = address;
+		this.dob = dob;
+
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public Date getDob() {
+		return this.dob;
+	}
+}

0a5592429483e923da304b0c76e7a3ee689520c9
Rishabh Shrivastava <rishabhshrivastava7565@gmail.com>
2025-09-15 14:58:06 +0530

    new

---
+++ Java-Collection/src/com/rays/collection/list/TestList.java
@@ -0,0 +1,34 @@
+package com.rays.collection.list;
+
+//1. List can contain multiple duplicate element
+//2. List can contain multiple null element
+//3. order of list is natural order
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TestList {
+	public static void main(String[] args) {
+		List list = new ArrayList();
+		list.add("BJP");
+		list.add('b');
+		list.add(56);
+		list.add(6.0);
+		list.add(null);
+		list.add(56);
+		list.add(null);
+		
+		System.out.println(list);
+		System.out.println(list.get(6));    //return the 6th index element  
+
+		list.remove(6);                  //remove the 6th element
+		System.out.println(list);
+		list.set(1, 'c');               //Replace the 1 index with objectc
+		System.out.println(list);
+		System.out.println(list.indexOf(56));       //search object from begining
+		System.out.println(list.lastIndexOf(6.0));  //search object from end
+		System.out.println(list.subList(2, 5));      //returns sublist
+	}
+
+}

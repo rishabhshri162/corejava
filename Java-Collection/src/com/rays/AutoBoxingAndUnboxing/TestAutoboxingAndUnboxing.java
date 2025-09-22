5cae640b598b28ad5a00d0587b3ad6bdd86edb0f
Rishabh Shrivastava <rishabhshrivastava7565@gmail.com>
2025-09-22 13:05:26 +0530

    new

--- Java-Collection/src/com/rays/AutoBoxingAndUnboxing/TestAutoboxingAndUnboxing.java
+++ Java-Collection/src/com/rays/AutoBoxingAndUnboxing/TestAutoboxingAndUnboxing.java
@@ -4,17 +4,17 @@ public class TestAutoboxingAndUnboxing {
 	public static void main(String[] args) {
 
 		System.out.println("----OLD METHOD----");
-		int a = 10;
-		Integer b = new Integer(a); // Autoboxing (means Data type into wrapper class)
+		int i = 10;
+		Integer j = new Integer(i); // Autoboxing (means Data type into wrapper class)
 
-		Integer t = b; // Unboxing (means Wrapper class into data type)
+		int k = j; // Unboxing (means Wrapper class into data type)
 
 		System.out.println("----NEW METHOD----");
 
-		int i = 10;
-		Integer k = i; // Autoboxing (means Data type into wrapper class)
+		int a = 10;
+		Integer b = i; // Autoboxing (means Data type into wrapper class)
 
-		Integer c = i; // Unboxing (means Wrapper class into data type)
+		int c = i; // Unboxing (means Wrapper class into data type)
 	}
 
 }

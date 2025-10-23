a2128ba4a3fe0aa0bc014b190f39967f5f094882
Rishabh Shrivastava <rishabhshrivastava7565@gmail.com>
2025-10-23 09:02:36 +0530

    done

---
+++ Scanner input example/src/com/rays/scanner/WordsCount.java
@@ -0,0 +1,17 @@
+package com.rays.scanner;
+
+import java.util.Scanner;
+
+public class WordsCount{
+	
+	  public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.print("Enter a sentence: ");
+	    String str = sc.nextLine();
+	    String[] words = str.split(" ");
+	    System.out.println("Word count: " + words.length);
+	  }
+	}
+
+
+
